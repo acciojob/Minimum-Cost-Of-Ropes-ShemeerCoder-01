926077418be98c2eeb03a0a208eaa7c405f7c872
ShemeerCoder-01 <118423467+ShemeerCoder-01@users.noreply.github.com>
2023-03-17 12:16:12 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,20 @@
 function calculateMinCost() {
-  //your code here
-  
-  
+	
+	var inputData = document.querySelector("#rope-lengths").value;
+	var inputArr = inputData.split(",");
+	for(var i = 0; i < inputArr.length; i++){
+		inputArr[i] = Number(inputArr[i]);
+	}
+	var cost = 0;
+	while(inputArr.length > 1){
+		inputArr.sort(function(a,b){return a-b;});
+		var rope = inputArr[0] + inputArr[1];
+		cost += rope;
+		inputArr.splice(0,2);
+		inputArr.push(rope);
+	}
+
+	document.getElementById("result").innerHTML = cost;
+
   
 }  
